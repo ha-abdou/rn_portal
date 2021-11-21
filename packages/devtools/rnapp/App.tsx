1beea64ffbf7a349a0d126f177afd3b635c3ae8f
hassaine abdel-illah <hassaineabdelillah@gmail.com>
2021-11-21 19:28:58 +0100

    set up workspaces done

---
+++ packages/devtools/rnapp/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: 'black'}}>
+      <Text>App</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ packages/devtools/rnapp/metro.config.js
@@ -0,0 +1,26 @@
+const getWorkspaces = require('get-yarn-workspaces');
+const path = require('path');
+
+function getConfig(appDir, options = {}) {
+  const workspaces = getWorkspaces(appDir);
+
+  const watchFolders = [
+    path.resolve(appDir, '../../..', 'node_modules'),
+    ...workspaces.filter(workspaceDir => !(workspaceDir === appDir)),
+  ];
+
+  return {
+    watchFolders,
+    transformer: {
+      experimentalImportSupport: false,
+      inlineRequires: true,
+    },
+    resolver: {
+      extraNodeModules: {
+        'react-native': path.resolve(appDir, 'node_modules', 'react-native'),
+      },
+    },
+  };
+}
+
+module.exports = getConfig(__dirname);
