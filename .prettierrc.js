f613ca1fb9a004aa74f4e9d3f5cfcad74c61c86c
hassaine abdel-illah <hassaineabdelillah@gmail.com>
2021-11-21 19:49:48 +0100

    prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+/** @format */
+
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  semi: false,
+  printWidth: 120,
+  insertPragma: true,
+  requirePragma: true,
+  proseWrap: 'always',
+}
