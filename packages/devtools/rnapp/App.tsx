a9569552ef270d92bc25d62b0def57cc1c657ef2
hassaine abdel-illah <hassaineabdelillah@gmail.com>
2021-11-29 14:41:22 +0100

    demo mobile app

--- packages/devtools/rnapp/App.tsx
+++ packages/devtools/rnapp/App.tsx
@@ -1,12 +1,43 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, { useRef } from 'react'
+import { SafeAreaView, View, Button } from 'react-native'
 
+import { creatPortal, IPortalRefType } from '@portal/core'
+import { loadReactNativeParsers } from '@portal/components'
+
+const url = 'ws://192.168.236.122:9100/phone'
+const protocols = 'echo-protocol'
+
+loadReactNativeParsers()
+const Portal = creatPortal({
+  wsParams: [url, protocols],
+})
 const App = () => {
-  return (
-    <SafeAreaView style={{flex: 1, backgroundColor: 'black'}}>
-      <Text>App</Text>
-    </SafeAreaView>
-  );
-};
+  const portalRef = useRef<IPortalRefType>(null)
+  const capture = () => {
+    if (!portalRef.current) return
+    portalRef.current
+      .capture()
+      .then((res) => {
+        console.log('res', res)
+      })
+      .catch((e) => {
+        console.log('error', e)
+      })
+  }
 
-export default App;
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: 'black',
+      }}
+    >
+      <Button title="capture" onPress={capture} />
+      <Portal ref={portalRef}>
+        <View style={{ width: 100, height: 100, backgroundColor: 'white' }} />
+      </Portal>
+    </SafeAreaView>
+  )
+}
+
+export default App
