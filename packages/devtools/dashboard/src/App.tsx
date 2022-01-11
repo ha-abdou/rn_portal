e82c689ef02f66e7607dffdad642d80f8648ace0
hassaine abdel-illah <hassaineabdelillah@gmail.com>
2022-01-11 23:03:56 +0100

    pre eject

--- packages/devtools/dashboard/src/App.tsx
+++ packages/devtools/dashboard/src/App.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { loadReactNativeRenderers } from '@portal/components'
+// import { createPortalExit } from '@portal/core'
+
+const url = 'ws://172.21.161.39:9100/dashboard'
+const protocols = 'echo-protocol'
+
+loadReactNativeRenderers()
+
+//const PortalExit = createPortalExit({
+//  wsParams: [url, protocols],
+//})
 
 function App() {
-  return <p>Dashboard</p>;
+
+  return <div>
+    
+  </div>;
 }
 
 export default App;
