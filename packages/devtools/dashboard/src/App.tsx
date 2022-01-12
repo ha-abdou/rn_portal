5ff3aa34e4f615beb96ff166d8b129f622d6a72e
hassaine abdel-illah <hassaineabdelillah@gmail.com>
2022-01-12 02:30:09 +0100

    fix: dashboard

--- packages/devtools/dashboard/src/App.tsx
+++ packages/devtools/dashboard/src/App.tsx
@@ -1,22 +1,22 @@
-import React, { useState } from 'react'
-
 import { loadReactNativeRenderers } from '@portal/components'
-// import { createPortalExit } from '@portal/core'
+import { createPortalExit } from '@portal/core'
 
 const url = 'ws://172.21.161.39:9100/dashboard'
 const protocols = 'echo-protocol'
 
 loadReactNativeRenderers()
 
-//const PortalExit = createPortalExit({
-//  wsParams: [url, protocols],
-//})
+const PortalExit = createPortalExit({
+  wsParams: [url, protocols],
+})
 
 function App() {
-
-  return <div>
-    
-  </div>;
+  return (
+    <div >
+      App
+      <PortalExit />
+    </div>
+  );
 }
 
 export default App;
