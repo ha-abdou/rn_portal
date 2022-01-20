78648e1d062de7070cdfab2cff08f3174dcaf847
hassaine abdel-illah <hassaineabdelillah@gmail.com>
2022-01-20 15:02:32 +0100

    feat: parser ready for build

--- packages/parser/index.ts
+++
@@ -1,7 +0,0 @@
-export { default as default } from './src/walker'
-
-export { addFiberParser } from './src/parseHandler'
-
-export { default as disableNativeDriver } from './src/disableNativeDriver'
-
-export * from './src/types'
---
+++ packages/parser/src/index.ts
@@ -0,0 +1,7 @@
+export { default as default } from './walker'
+
+export { addFiberParser } from './parseHandler'
+
+export { default as disableNativeDriver } from './disableNativeDriver'
+
+export * from './types'
